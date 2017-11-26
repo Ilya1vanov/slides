68b2f3f15c5cdfd798b2e77e71b9654673890949
Ilya1vanov <com.ilya.ivanov@gmail.com>
2017-11-26 15:44:23 +0300

    Add presentation CRUD controller

---
+++ backend/src/main/java/com/ilya/ivanov/slides/controller/resources/PresentationController.java
@@ -0,0 +1,77 @@
+package com.ilya.ivanov.slides.controller.resources;
+
+import com.ilya.ivanov.slides.constants.JwtConstants;
+import com.ilya.ivanov.slides.data.model.domain.presentation.Presentation;
+import com.ilya.ivanov.slides.data.model.domain.user.User;
+import com.ilya.ivanov.slides.data.model.dto.presentation.PresentationDto;
+import com.ilya.ivanov.slides.data.repository.PresentationRepository;
+import com.ilya.ivanov.slides.exception.ResourceNotFoundException;
+import com.ilya.ivanov.slides.service.PresentationService;
+import com.ilya.ivanov.slides.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toList;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
+/**
+ * Created by i.ivanov on 11/24/17.
+ */
+@RestController
+@RequestMapping(value = "/resources/presentation")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@PreAuthorize("hasAuthority('STANDARD_USER')")
+@Api(value = "/resources/presentation")
+public class PresentationController {
+    private final PresentationService presentationService;
+
+    @Value("${security.jwt.scope-read}")
+    private String scopeRead;
+
+    @GetMapping("/all")
+    @PreAuthorize("#oauth2.hasScope(T(com.ilya.ivanov.slides.constants.JwtConstants).scopeRead)")
+    @ApiOperation(response = PresentationDto.class, responseContainer="List", value = "Return All user presentations repr")
+    @ApiResponse(code = SC_OK, message = "Presentation returned")
+    public Collection<PresentationDto> getPresentations() {
+        return presentationService.getAllPresentations().stream()
+                .map(Presentation::toDto)
+                .collect(toList());
+    }
+
+    @PostMapping("/")
+    @PreAuthorize("#oauth2.hasScope(T(com.ilya.ivanov.slides.constants.JwtConstants).scopeWrite)")
+    @ApiOperation(response = PresentationDto.class, value = "Return Presentation repr")
+    @ApiResponse(code = SC_OK, message = "Presentation created")
+    public PresentationDto createPresentation(@ApiParam @RequestBody @Valid PresentationDto presentationDto) {
+        return Presentation.toDto(presentationService.addPresentation(presentationDto));
+    }
+
+    @PatchMapping("/")
+    @PreAuthorize("#oauth2.hasScope(T(com.ilya.ivanov.slides.constants.JwtConstants).scopeWrite)")
+    @ApiOperation(response = PresentationDto.class, responseContainer="List", value = "Update presentation")
+    @ApiResponse(code = SC_OK, message = "Presentation updated")
+    public PresentationDto editPresentation(@ApiParam @RequestBody @Valid PresentationDto presentationDto) {
+        return Presentation.toDto(presentationService.editPresentation(presentationDto));
+    }
+
+    @DeleteMapping("/")
+    @PreAuthorize("#oauth2.hasScope(T(com.ilya.ivanov.slides.constants.JwtConstants).scopeWrite)")
+    @ApiOperation(response = PresentationDto.class, responseContainer="List", value = "Remove presentation")
+    @ApiResponse(code = SC_OK, message = "Presentation removed")
+    public void deletePresentation(@ApiParam @RequestBody @Valid PresentationDto presentationDto) {
+        presentationService.removePresentation(presentationDto);
+    }
+}
