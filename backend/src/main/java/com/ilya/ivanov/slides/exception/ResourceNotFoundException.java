952bae1192cc500319315528d1ea15ce4fb8bf31
Ilya1vanov <com.ilya.ivanov@gmail.com>
2017-11-26 15:41:29 +0300

    Add custom exception

---
+++ backend/src/main/java/com/ilya/ivanov/slides/exception/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package com.ilya.ivanov.slides.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by i.ivanov on 11/25/17.
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException() {
+        super("Resource not found");
+    }
+
+    public ResourceNotFoundException(String resourceName, Long id) {
+        super(resourceName + " with id " + id + " not found.");
+    }
+}
