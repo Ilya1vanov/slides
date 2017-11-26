5f5ceebf56a84f518da2d1686988be99b4b179bc
Ilya1vanov <com.ilya.ivanov@gmail.com>
2017-11-26 15:40:10 +0300

    Add user id to the token

---
+++ backend/src/main/java/com/ilya/ivanov/slides/utils/SecurityUtils.java
@@ -0,0 +1,31 @@
+package com.ilya.ivanov.slides.utils;
+
+import com.ilya.ivanov.slides.constants.JwtConstants;
+import com.ilya.ivanov.slides.data.model.domain.user.User;
+import com.ilya.ivanov.slides.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by i.ivanov on 11/25/17.
+ */
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SecurityUtils {
+
+    private final TokenStore tokenStore;
+
+    private final JwtConstants jwtConstants;
+
+    public Long getUserId() {
+        val details = (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails();
+        val accessToken = tokenStore.readAccessToken(details.getTokenValue());
+        return Long.parseLong(accessToken.getAdditionalInformation().get(jwtConstants.getUserIdKey()).toString());
+    }
+}
